1e267ac78afca320b8abc83dae5c903ed5902fea
Deepak Kumar <deepaksibar@gmail.com>
2016-09-09 18:28:02 +0530

    Update systemjs.config.js

--- systemjs.config.js
+++ systemjs.config.js
@@ -6,12 +6,12 @@
 	System.config({
 		paths : {
 			// paths serve as alias
-			'npm:' : 'node_modules/'
+			'npm:' 								: 'node_modules/'
 		},
 		// map tells the System loader where to look for things
 		map : {
 			// our app is within the app folder
-			app : 'src/main/webapp',
+			app 								: 'src/main/webapp',
 			// angular bundles
 			'@angular/core' 					: 'npm:@angular/core/bundles/core.umd.js',
 			'@angular/common' 					: 'npm:@angular/common/bundles/common.umd.js',
@@ -22,14 +22,14 @@
 			'@angular/router' 					: 'npm:@angular/router/bundles/router.umd.js',
 			'@angular/forms' 					: 'npm:@angular/forms/bundles/forms.umd.js',
 			// other libraries
-			'rxjs' : 'npm:rxjs',
-			'angular2-in-memory-web-api' : 'npm:angular2-in-memory-web-api',
+			'rxjs' 								: 'npm:rxjs',
+			'angular2-in-memory-web-api' 		: 'npm:angular2-in-memory-web-api',
 		},
 		// packages tells the System loader how to load when no filename and/or no extension
 		packages : {
-			app 						: {main : './main.js',defaultExtension : 'js'},
-			rxjs 						: {defaultExtension : 'js'},
-			'angular2-in-memory-web-api': {main : './index.js',defaultExtension : 'js'}
+			app 								: {main : './main.js',defaultExtension : 'js'},
+			rxjs 								: {defaultExtension : 'js'},
+			'angular2-in-memory-web-api'		: {main : './index.js',defaultExtension : 'js'}
 		}
 	});
 })(this);
